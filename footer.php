602477c5f31340b43677f546768caedd62c6a573
Stephen Karpeles <stephen@stephenkarpeles.com>
2019-12-06 12:05:04 -0600

    Base styles

--- footer.php
+++ footer.php
@@ -14,19 +14,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'spiro-medicine' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'spiro-medicine' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'spiro-medicine' ), 'spiro-medicine', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
---
+++ page-homepage.php
@@ -0,0 +1,13 @@
+<?php 
+/* Template Name: Homepage */ 
+get_header(); 
+?>
+
+<div class="grid-row">
+  <div class="col col-1-4"><p>Test</p></div>
+  <div class="col col-1-4"><p>Test</p></div>
+  <div class="col col-1-4"><p>Test</p></div>
+  <div class="col col-1-4"><p>Test</p></div>
+</div>
+
+<?php get_footer(); ?>
