67c9e7d8901659df11fc342380d12fb95ec60487
Stephen Karpeles <stephen@stephenkarpeles.com>
2020-01-07 22:46:56 -0600

    About page

--- page-about.php
+++ page-about.php
@@ -16,9 +16,60 @@ get_header();
   </div>
 </section>
 
-<section></section>
-<section></section>
-<section></section>
+<section>
+  <div class="container">
+
+    <div class="grid-row mar-bot-30">
+      <div class="col col-full-width text-center">
+        <h2>Who Are <span class="text-green">We</span>?</h2>
+      </div>
+    </div>
+
+    <div class="grid-row">
+      <div class="col col-1-2">
+        <p>At Spiro Behavioral Medicine our mission is to provide evidence-based, specialized, hyperaccessible psychotherapy and consultation related to infertility and other reproductive
+        concerns. Our goal is to help our clients, their partners, family, and third parties be their most resilient during the process of building a family or making the decision to proceed in another direction. To achieve this end, our therapists, who have a deep grounding in scientificallybased psychotherapy and specialized knowledge of reproductive mental health, provide individual and group counseling.</p>
+      </div>
+      <div class="col col-1-2">
+        <p>
+          To ensure that the highest quality of care is
+          widely available and convenient during a period in life with many impingements on time and energy, we offer these services through a stateof- the-art, web-based platform that mimics an in-person, individual psychotherapy session without the hassle or expense of travel to an appointment. While some of our therapists have gone through our own winding journeys to build a family, we recognize that each journey is unique, and look forward to providing support that is at once empathetic, non-judgmental, based in evidence, and couched in deep knowledge of the conceptual, psychological underpinnings related to reproductive health.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="section-alt-bg">
+  <div class="container">
+
+    <div class="grid-row mar-bot-30">
+      <div class="col col-full-width text-center">
+        <h2>Our <span class="text-green">Team</span></h2>
+      </div>
+    </div>
+
+    <div class="grid-row">
+      <div class="col col-1-2">
+        <h3 class="text-center mar-bot-10">
+          Sarah Cantanese
+        </h3>
+        <p>Dr. Catanese has nearly 15 years of experience as a clinical psychologist. She believes in the transformative power of psychotherapy, through a blend of art and science, to help each client reach their potential, even, and sometimes especially, during challenging times. "Each individual has his or her own path, and the art of psychotherapy comes in adapting knowledge, gained through training and science, to each person’s unique goals."</p>
+        <p>
+          Dr. Catanese completed a post-doctoral fellowship in clinical psychology and research at Children’s Hospital Boston/Harvard Medical School. She completed her pre-doctoral training at UCLA Resnick Neuropsychiatric Hospital, Department of Psychiatry and Behavioral Sciences, David Geffen School of Medicine. She completed her PhD in clinical psychology, with a specialization in Health Psychology, at Rosalind Franklin University of Health Sciences, a nationally recognized scientist-practitioner program.
+        </p>
+      </div>
+      <div class="col col-1-2">
+        <h3 class="text-center mar-bot-10">
+          Sarah Cantanese
+        </h3>
+        <p>Dr. Catanese has nearly 15 years of experience as a clinical psychologist. She believes in the transformative power of psychotherapy, through a blend of art and science, to help each client reach their potential, even, and sometimes especially, during challenging times. "Each individual has his or her own path, and the art of psychotherapy comes in adapting knowledge, gained through training and science, to each person’s unique goals."</p>
+        <p>
+          Dr. Catanese completed a post-doctoral fellowship in clinical psychology and research at Children’s Hospital Boston/Harvard Medical School. She completed her pre-doctoral training at UCLA Resnick Neuropsychiatric Hospital, Department of Psychiatry and Behavioral Sciences, David Geffen School of Medicine. She completed her PhD in clinical psychology, with a specialization in Health Psychology, at Rosalind Franklin University of Health Sciences, a nationally recognized scientist-practitioner program.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php include('inc/mailing-list-panel.php'); ?>
 
