080bf225e4e890c6fa541ab7b27d8fa29ab68680
Stephen Karpeles <stephen@stephenkarpeles.com>
2020-01-08 21:47:06 -0600

    Resources page

--- page-resources.php
+++ page-resources.php
@@ -16,6 +16,114 @@ get_header();
   </div>
 </section>
 
+<section class="section-extra-padding">
+  <div class="container">
+
+    <div class="grid-row grid-row--underlined">
+
+      <div class="col col-1-2">
+        <h4 class="heading-alt heading-alt--a">
+          Suicidal Thoughts
+        </h4>
+        <p>Here are some resources that we recommend taking a look at:</p>
+        <p>
+          <strong>Suicide Prevention Hotline</strong><br>
+          <a href="https://suicidepreventionlifeline.org/talk-tosomeone-now/">https://suicidepreventionlifeline.org/talk-tosomeone-now/</a>
+        </p>
+        <p>
+          <strong>Suicide Prevention Hotline</strong><br>
+          <a href="https://suicidepreventionlifeline.org/talk-tosomeone-now/">https://suicidepreventionlifeline.org/talk-tosomeone-now/</a>
+        </p>
+      </div>
+
+      <div class="col col-1-2">
+        <h4 class="heading-alt heading-alt--b">
+          Emergency Hotlines
+        </h4>
+        <p>If you or someone you know is having suicidal thoughts, please call below:</p>
+        <p>
+          <strong>Suicide Hotline:</strong> 800-273-8255 <br>
+          (Available 24 hours everyday)
+        </p>
+        <p>
+          <strong>Suicide Hotline:</strong> 800-273-8255 <br>
+          (Available 24 hours everyday)
+        </p>        
+      </div>
+
+    </div>
+
+    <div class="grid-row grid-row--underlined">
+
+      <div class="col col-1-2">
+        <h4 class="heading-alt heading-alt--a">
+          Suicidal Thoughts
+        </h4>
+        <p>Here are some resources that we recommend taking a look at:</p>
+        <p>
+          <strong>Suicide Prevention Hotline</strong><br>
+          <a href="https://suicidepreventionlifeline.org/talk-tosomeone-now/">https://suicidepreventionlifeline.org/talk-tosomeone-now/</a>
+        </p>
+        <p>
+          <strong>Suicide Prevention Hotline</strong><br>
+          <a href="https://suicidepreventionlifeline.org/talk-tosomeone-now/">https://suicidepreventionlifeline.org/talk-tosomeone-now/</a>
+        </p>
+      </div>
+
+      <div class="col col-1-2">
+        <h4 class="heading-alt heading-alt--b">
+          Emergency Hotlines
+        </h4>
+        <p>If you or someone you know is having suicidal thoughts, please call below:</p>
+        <p>
+          <strong>Suicide Hotline:</strong> 800-273-8255 <br>
+          (Available 24 hours everyday)
+        </p>
+        <p>
+          <strong>Suicide Hotline:</strong> 800-273-8255 <br>
+          (Available 24 hours everyday)
+        </p>        
+      </div>
+
+    </div>
+
+    <div class="grid-row grid-row--underlined">
+
+      <div class="col col-1-2">
+        <h4 class="heading-alt heading-alt--a">
+          Suicidal Thoughts
+        </h4>
+        <p>Here are some resources that we recommend taking a look at:</p>
+        <p>
+          <strong>Suicide Prevention Hotline</strong><br>
+          <a href="https://suicidepreventionlifeline.org/talk-tosomeone-now/">https://suicidepreventionlifeline.org/talk-tosomeone-now/</a>
+        </p>
+        <p>
+          <strong>Suicide Prevention Hotline</strong><br>
+          <a href="https://suicidepreventionlifeline.org/talk-tosomeone-now/">https://suicidepreventionlifeline.org/talk-tosomeone-now/</a>
+        </p>
+      </div>
+
+      <div class="col col-1-2">
+        <h4 class="heading-alt heading-alt--b">
+          Emergency Hotlines
+        </h4>
+        <p>If you or someone you know is having suicidal thoughts, please call below:</p>
+        <p>
+          <strong>Suicide Hotline:</strong> 800-273-8255 <br>
+          (Available 24 hours everyday)
+        </p>
+        <p>
+          <strong>Suicide Hotline:</strong> 800-273-8255 <br>
+          (Available 24 hours everyday)
+        </p>        
+      </div>
+
+    </div>
+
+  </div>
+</section>
+
 <?php include('inc/contact-form-panel.php'); ?>
 
 <?php get_footer(); ?>
